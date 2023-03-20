6a4f9a1d1ce887b5b19f16d256b8cc01fe7c2bb7
raman <r4m4n911@gmail.com>
2023-03-20 21:37:48 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
         int i = 3;
-        while (i < printToInclusive / 2) {
+        while (i < printToInclusive) {
             int j = 2;
             while (j < i) {
                 if (i % j == 0) {
