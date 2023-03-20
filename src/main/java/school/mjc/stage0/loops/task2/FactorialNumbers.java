5dc8e2fc1e77d4cefe7ed8632fc231a18b9852cc
raman <r4m4n911@gmail.com>
2023-03-20 21:38:47 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -4,7 +4,7 @@ public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
         int i = 1;
         int j = 1;
-        while (i < printToInclusive + 2) {
+        while (i <= printToInclusive ) {
             System.out.println(j);
             j *= i;
             i++;
