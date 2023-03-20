121fb67c38831147b6c1486f1d53079afb2c0aaf
raman <r4m4n911@gmail.com>
2023-03-20 21:14:02 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int i = 1;
+        int j = 1;
+        while (j < power + 1) {
+            System.out.println(i);
+            i *= 2;
+            i++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,9 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
-        int i = 1;
+        int i = 3;
         while (i < printToInclusive / 2) {
-
+            int j = 2
+            while (j < i) {
+                if (i % j == 0) {
+                    break;
+                }
+                j++;
+            }
+            if (i == j) {
+                System.out.println(i);
+            }
+            i++;
         }
     }
 }
